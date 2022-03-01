45ab9eda1cb6ab1bc645e4aa5c420d306e91b853
Denis <kolosov@huntflow.ru>
2022-03-01 10:40:42 +0700

    fix: remove logic about anyOf/oneOf

--- src/components/fields/SchemaField.vue
+++ src/components/fields/SchemaField.vue
@@ -63,12 +63,6 @@ export default {
     fieldEventListeners() {
       return pick(this.$listeners, ['drop-property', 'key-change', 'focus', 'blur', 'change']);
     },
-    anyOfFieldEventListeners() {
-      return pick(this.$listeners, ['focus', 'blur', 'change']);
-    },
-    oneOfFieldEventListeners() {
-      return pick(this.$listeners, ['focus', 'blur', 'change']);
-    },
     hasAutofocus() {
       return Boolean(this.autofocus || this.uiSchema['ui:autofocus']);
     },
