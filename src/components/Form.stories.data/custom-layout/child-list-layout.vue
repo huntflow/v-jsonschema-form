ff8461182bce620d6553cfe307029bbfb3274b66
leonied7 <kdnn@mail.ru>
2023-08-25 12:20:25 +0700

    drop styles

--- src/components/Form.stories.data/custom-layout/child-list-layout.vue
+++ src/components/Form.stories.data/custom-layout/child-list-layout.vue
@@ -3,19 +3,3 @@
     <slot name="default"> </slot>
   </div>
 </template>
-
-<!--<style lang="less" module>-->
-<!--.list :global(.array-item) {-->
-<!--  overflow: auto;-->
-
-<!--  & > * {-->
-<!--    &:first-child {-->
-<!--      width: 100%;-->
-<!--      padding: 0;-->
-<!--    }-->
-<!--    &:last-child {-->
-<!--      display: none;-->
-<!--    }-->
-<!--  }-->
-<!--}-->
-<!--</style>-->
